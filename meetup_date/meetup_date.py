c2d68ccb9b96042970b119ea58f6cbfda5020081
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2021-10-22 16:11:08 -0600

    Starter code and tests for meetup_date puzzle

---
+++ meetup_date/meetup_date.py
@@ -0,0 +1,7 @@
+import datetime
+
+
+def meetup_date(year: int, month: int) -> datetime.date:
+    """Returns the date of the San Diego Python meetup for the input year and month
+    Meetups occur every fourth Thursday of the month."""
+    pass
